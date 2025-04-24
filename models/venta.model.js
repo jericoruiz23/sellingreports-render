25b03d1d70529144f6043c7a82ce8d984350e301
Benjamin Ruiz <preventa10.ec@nexsysla.com>
2025-04-24 16:56:10 -0500

    Table consume

--- models/venta.model.js
+++ models/venta.model.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 const ventaSchema = new mongoose.Schema({
     producto: { type: String, required: true },
-    cantidad: { type: Number, required: true },
-    precio: { type: Number, required: true },
+    marca: { type: String, required: true },
+    modelo: { type: String, required: true },
+    procesador: { type: String, required: true },
+    generacion: { type: String, required: true },
+    ram: { type: String, required: true },
+    almacenamiento: { type: String, required: true },
+    capacidad: { type: String, required: true },
+    graficos: { type: String, required: true },
     fecha: { type: Date, default: Date.now }
 });
 
