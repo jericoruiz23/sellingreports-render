227b4a271cceaa2685673afcbd160b8662ae8b6a
Benjamin Ruiz <preventa10.ec@nexsysla.com>
2025-04-28 17:08:23 -0500

    DB RESTRICTION

--- server.js
+++ server.js
@@ -1,29 +1,36 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-
+const mongoose = require('mongoose');
 const ventasRoutes = require('./routes/ventas.routes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middlewares
 app.use(express.json());
-
-const cors = require('cors');
-
 app.use(cors({
     origin: 'https://jericoruiz23.github.io', // o usa una función si quieres varios orígenes
     methods: ['GET', 'POST'],
     credentials: false,
-  }));
-  
+}));
+
 
 // Rutas
 app.use('/ventas', ventasRoutes);
 
 app.get('/', (req, res) => {
     res.json({ message: 'API funcionando 🚀' });
+});
+
+app.get('/prueba-db', async (req, res) => {
+    try {
+        const resultado = await mongoose.connection.db.listCollections().toArray();
+        res.json(resultado);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 // Conectar a DB y levantar servidor
